f00309d28bdc45af108d0fbdb05611a0a9d9c3a6
Lars Åge Kamfjord <lars-github@kamfjord.org>
2013-12-29 20:49:47 +0100

    Changing to gettext

--- core/views.py
+++ core/views.py
@@ -1,9 +1,9 @@
 # Create your views here.
 from django.shortcuts import render
-
+from django.utils.translation import ugettext as _
 
 
 def index(request):
 	c = {}
-	c['main'] = 'Hei verden'
+	c['main'] = _('Hello world')
 	return render(request, "index.html", c)
