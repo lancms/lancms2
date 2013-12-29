5794ae465bbe9a88ad739f9e6c7e0c92a17c5559
Lars Åge Kamfjord <lars-github@kamfjord.org>
2013-12-29 23:40:01 +0100

    extending tickettypes with more information

--- ticket/models.py
+++ ticket/models.py
@@ -1,13 +1,25 @@
 from django.db import models
 from core.models import Event
 from django.contrib.auth.models import User
-
+from django.utils.translation import pgettext_lazy as _
 
 # Create your models here.
 
 class TicketType(models.Model):
 	Event = models.ForeignKey(Event)
-	name = models.CharField(max_length=35)
+	Name = models.CharField(max_length=35)
+	ValidSoldFrom = models.DateTimeField(blank=True)
+	ValidSoldUntil = models.DateTimeField(blank=True)
+	TypeChoice = (
+		('onsite-noncomputer', 'Onsite: Without computer'),
+		('onsite-withcomputer', 'Onsite: With computer'),
+		('prepaid', 'Prepaid'),
+		('preorder', 'Preorder'),
+		('special-crew', 'Crew'),
+		('special-invitation', 'Special invitation'),
+	)
+	Type = models.CharField(max_length=15, choices=TypeChoice)
+#	Type = models.CharField(max_length=15)
 
 	def __unicode__(self):
 		return self.name
