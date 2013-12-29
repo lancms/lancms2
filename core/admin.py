2500c6a699253976fae4680d69eb1261bd8c5a75
Lars Åge Kamfjord <lars-github@kamfjord.org>
2013-12-29 21:43:07 +0100

    Added userprofile with extra information about users/extending User-model

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,16 @@
-from core.models import Event
+from core.models import Event, UserProfile
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
+class UserProfileInline(admin.StackedInline):
+	model = UserProfile
+	can_delete = False
+
+class UserAdmin(UserAdmin):
+	inlines = (UserProfileInline, )
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Event)
--- core/models.py
+++ core/models.py
@@ -1,8 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 # Create your models here.
 
 class Event(models.Model):
 	name = models.CharField(max_length=64)
 
+	class Meta:
+		permissions = (
+			("add", "Add"),
+			("change", "Change"),
+		)
 
+class UserProfile(models.Model):
+	user = models.OneToOneField(User)
+	birthday = models.DateField ()
+	postnumber = models.CharField(max_length=128)
+	address = models.CharField(max_length=128)
+	cellphone = models.CharField(max_length=10)
+	facebookID = models.IntegerField(max_length=15,blank=True)
+	registerIP = models.GenericIPAddressField()
+	profilePicture = models.FileField(upload_to='profilePicture/', blank=True)
