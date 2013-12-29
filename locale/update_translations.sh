b04d98666aa8aa29753cb661ccf083b02b423e4c
Lars Åge Kamfjord <lars-github@kamfjord.org>
2013-12-29 22:23:59 +0100

    Creating a pot-file

--- locale/update_translations.sh
+++ locale/update_translations.sh
@@ -1,1 +1,3 @@
-./manage.py makemessages --all -e html -e py -l en-us
+xgettext -i */*.py -i -klang -o locale/messages.pot --from-code=UTF-8 --copyright-holder="LANCMS and it's creators" --package-name='LANCMS' --msgid-bugs-address='bugs.launchpad.net' --package-version='2.0-alpha'
+
+#./manage.py makemessages --all -e html -e py -l en-us
