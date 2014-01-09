70f0dab88d33babc3825161f5697b8f559e3facf
Mathias Bøhn Grytemark <mathias@grytemark.no>
2014-01-09 19:12:37 +0100

    fixing tab

--- core/common.py
+++ core/common.py
@@ -2,4 +2,6 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 def prtr (template, dict, request):
-    return render_to_response (template, dict, context_instance=RequestContext(request))
+	return render_to_response (template, dict, context_instance=RequestContext(request))
+
+
