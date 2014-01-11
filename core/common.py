5d676b7c8c6cb74cc4dfd2243c403f6390b118ec
Mathias Bøhn Grytemark <mathias@grytemark.no>
2014-01-11 20:24:16 +0100

    bootstrapified email-verification templates too

--- core/common.py
+++ core/common.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
-def prtr (template, dict, request):
-	return render_to_response (template, dict, context_instance=RequestContext(request))
+from django.conf import settings
+
+def prtr (template, c, request):
+
+	try:
+		if settings.DEBUG_WARN:
+			c['DEBUG_WARN'] = True
+	except:
+		pass
+
+	return render_to_response (template, c, context_instance=RequestContext(request))
 
 
