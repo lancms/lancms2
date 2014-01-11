e8c65a19020989feca871662e71471fcff1d3827
Lars Åge Kamfjord <lars-github@kamfjord.org>
2014-01-11 20:46:09 +0100

    Trying to use django-admin makemessages with symbolink links

--- locale/update_translations.sh
+++ locale/update_translations.sh
@@ -1,3 +1,7 @@
-xgettext -i */*.py -i -klang -o locale/messages.pot --from-code=UTF-8 --copyright-holder="LANCMS and it's creators" --package-name='LANCMS' --msgid-bugs-address='bugs.launchpad.net' --package-version='2.0-alpha'
+
+
+
+#xgettext -i */*.py -i -klang -o locale/messages.pot --from-code=UTF-8 --copyright-holder="LANCMS and it's creators" --package-name='LANCMS' --msgid-bugs-address='bugs.launchpad.net' --package-version='2.0-alpha'
 
 #./manage.py makemessages --all -e html -e py -l en-us
+./manage.py makemessages -l en_US -d django -s
