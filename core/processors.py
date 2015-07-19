043eaf6800fa2e17b6b3e7dd06ca4888711278bb
Mathias Bøhn Grytemark <mathias@grytemark.no>
2015-07-19 21:47:20 +0200

    Adding the DEBUG-settings used for dev-sites

--- core/processors.py
+++ core/processors.py
@@ -2,7 +2,23 @@
 
 from core.models import Organization
 
-def lc_data(request):
-	organizations = Organization.objects.filter(is_active=True)
+from django.conf import settings
 
-	return {'organizations': organizations}
+def lc_data(request):
+	c = {}
+
+	c['organizations'] = Organization.objects.filter(is_active=True)
+
+	try:
+		if settings.DEBUG_WAR:
+			c['DEBUG_WARN'] = True
+	except:
+		pass
+	
+	try:
+		if settings.DEBUG_REV:
+			c['DEBUG_REV'] = settings.DEBUG_REV
+	except:
+		pass
+
+	return c
