c221c87f44c764859ab61d6a14ac538fdaea782d
Mathias Bøhn Grytemark <mathias@grytemark.no>
2015-07-31 11:26:41 +0200

    Add menu of events
    Same as for organizations. Show only one if there is only one.
    Added all_events to lc_data-context processor.

--- core/processors.py
+++ core/processors.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 
-from event.models import Organization
+from event.models import Organization, Event
 
 def lc_data(request):
     c = {}
-    
+
     # FIXME: should filter on some active-flag
     c['all_organizations'] = Organization.objects.all()
+
+    # FIXME: should filter on some active-flag
+    c['all_events'] = Event.objects.all()
 
     return c
